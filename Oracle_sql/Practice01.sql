d3723d8c69c73f498087a299468f17807241e955
ChangsKIM <kcs7601@gmail.com>
2024-10-17 00:12:11 +0900

    복습 group by, NEW_STUDENT

--- Oracle_sql/Practice01.sql
+++ Oracle_sql/Practice01.sql
@@ -35,6 +35,23 @@ SELECT
 	WHERE STUDENT.STD_SCORE < 2.8
 	GROUP BY STD_MAJOR ;
 
+-- 현재 학생 테이블에 있는 데이터를 기준으로 학과별 인원수를 조회
+-- 단, 조회하는 인원수가 50명 이상인 학과만 출력
+SELECT STD_MAJOR AS 학과명, COUNT(*)
+FROM STUDENT
+GROUP BY STD_MAJOR HAVING COUNT(*) >= 50;
+
+
+-- NEW_STUDENT TABLE 생성
+CREATE TABLE NEW_STUDENT(
+    STD_NO CHAR(8) PRIMARY KEY,
+    STD_NAME VARCHAR2(30) NOT NULL,
+    STD_MAJOR VARCHAR2(30),
+    STD_SCORE NUMBER(3,2) DEFAULT 0 NOT NULL,
+    STD_GENDER CHAR(1)
+);
+
+
 --연습문제
 --입학한 년도별, 학과별, 성별로 인원수, 평점 평균, 평점 총합를 조회하세요.
 
